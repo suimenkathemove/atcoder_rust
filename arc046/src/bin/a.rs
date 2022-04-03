db0a58e67bee298c7a4d6a52033037b41e9ecd58
emgniddikur <yoshiharu2580@gmail.com>
2022-04-03 11:44:52 +0900

    arc046 a

--- arc046/src/bin/a.rs
+++ arc046/src/bin/a.rs
@@ -1,3 +1,19 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+    }
+
+    let quotient = n / 9;
+    let remainder = n % 9;
+
+    let digit = if remainder == 0 {
+        quotient
+    } else {
+        quotient + 1
+    };
+    let number = if remainder == 0 { 9 } else { remainder };
+
+    let answer = (0..digit).map(|_| number.to_string()).collect::<String>();
+
+    println!("{}", answer);
 }
