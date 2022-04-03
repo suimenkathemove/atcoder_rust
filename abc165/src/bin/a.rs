53aaecd25be70c883aa60c0154732663c40b5773
emgniddikur <yoshiharu2580@gmail.com>
2022-04-04 03:41:51 +0900

    abc165 a

--- abc165/src/bin/a.rs
+++ abc165/src/bin/a.rs
@@ -1,3 +1,19 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        k: usize,
+        a: usize,
+        b: usize,
+    }
+
+    let mut answer = false;
+
+    for i in a..=b {
+        if i % k == 0 {
+            answer = true;
+
+            break;
+        }
+    }
+
+    println!("{}", if answer { "OK" } else { "NG" });
 }
