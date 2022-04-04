c708f44755373fa0868c8b93d6a6fba71a3a4084
emgniddikur <yoshiharu2580@gmail.com>
2022-04-05 04:18:04 +0900

    past202004-open d

--- past202004-open/src/bin/d.rs
+++ past202004-open/src/bin/d.rs
@@ -1,3 +1,44 @@
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        S: String,
+    }
+
+    let mut set: HashSet<String> = HashSet::new();
+
+    for start in 0..=S.len() - 1 {
+        for gap in 0..=2 {
+            let end = start + gap;
+
+            if end > S.len() - 1 {
+                break;
+            }
+
+            let base_str = &S[start..=end];
+
+            fn combine_with_colon(
+                current_str: &str,
+                base_str: &str,
+                index: usize,
+                set: &mut HashSet<String>,
+            ) {
+                if index > base_str.len() - 1 {
+                    return;
+                }
+
+                let normal_string =
+                    format!("{}{}", current_str, base_str.chars().nth(index).unwrap());
+                set.insert(normal_string.clone());
+                combine_with_colon(&normal_string, base_str, index + 1, set);
+
+                let colon_string = format!("{}{}", current_str, '.');
+                set.insert(colon_string.clone());
+                combine_with_colon(&colon_string, base_str, index + 1, set);
+            }
+            combine_with_colon("", base_str, 0, &mut set);
+        }
+    }
+
+    println!("{}", set.len());
 }
