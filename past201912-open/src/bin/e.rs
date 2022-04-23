a29c65c970cc86098439235fabab5907d27b3a3a
emgniddikur <yoshiharu2580@gmail.com>
2022-04-23 19:01:49 +0900

    past201912-open e

--- past201912-open/src/bin/e.rs
+++ past201912-open/src/bin/e.rs
@@ -1,3 +1,61 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        N: usize,
+        Q: usize,
+    }
+
+    let mut g = vec![vec![false; N]; N];
+
+    (0..Q).for_each(|_| {
+        proconio::input! {
+            t: usize,
+            a: usize,
+        }
+
+        let a_index = a - 1;
+
+        match t {
+            1 => {
+                proconio::input! {
+                    b: usize,
+                }
+
+                let b_index = b - 1;
+
+                g[a_index][b_index] = true;
+            }
+            2 => {
+                (0..N).for_each(|i| {
+                    if g[i][a_index] {
+                        g[a_index][i] = true;
+                    }
+                });
+            }
+            3 => {
+                let mut followings: Vec<usize> = vec![];
+
+                (0..N).for_each(|i| {
+                    if g[a_index][i] {
+                        (0..N).for_each(|j| {
+                            if g[i][j] && j != a_index {
+                                followings.push(j);
+                            }
+                        });
+                    }
+                });
+
+                followings.iter().for_each(|&i| {
+                    g[a_index][i] = true;
+                });
+            }
+            _ => (),
+        }
+    });
+
+    (0..N).for_each(|i| {
+        let s = (0..N).fold("".to_string(), |acc, j| {
+            acc + if g[i][j] { "Y" } else { "N" }
+        });
+        println!("{}", s);
+    });
 }
