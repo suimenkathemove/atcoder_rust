c662d6e518a137708bd966f8056ad7a8a7cd0518
emgniddikur <yoshiharu2580@gmail.com>
2022-05-30 10:09:18 +0900

    abc114 c

--- abc114/src/bin/c.rs
+++ abc114/src/bin/c.rs
@@ -1,3 +1,36 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+    }
+
+    let mut answer = 0;
+
+    fn dfs(
+        n: usize,
+        answer: &mut usize,
+        current: usize,
+        new: usize,
+        includes_3: bool,
+        includes_5: bool,
+        includes_7: bool,
+    ) {
+        let new_current: usize = (current.to_string() + &new.to_string()).parse().unwrap();
+
+        if new_current > n {
+            return;
+        }
+
+        if includes_3 && includes_5 && includes_7 {
+            *answer += 1;
+        }
+
+        dfs(n, answer, new_current, 3, true, includes_5, includes_7);
+        dfs(n, answer, new_current, 5, includes_3, true, includes_7);
+        dfs(n, answer, new_current, 7, includes_3, includes_5, true);
+    }
+    dfs(n, &mut answer, 0, 3, true, false, false);
+    dfs(n, &mut answer, 0, 5, false, true, false);
+    dfs(n, &mut answer, 0, 7, false, false, true);
+
+    println!("{}", answer);
 }
