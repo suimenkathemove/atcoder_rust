122701eda9b3163e240ac9c176d2af5ac86fa30c
emgniddikur <yoshiharu2580@gmail.com>
2022-06-04 07:26:49 +0900

    dp a

--- dp/src/bin/a.rs
+++ dp/src/bin/a.rs
@@ -1,3 +1,19 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        h: [usize; n],
+    }
+
+    let mut cost = vec![0; n];
+
+    cost[1] = (h[0] as isize - h[1] as isize).abs() as usize;
+
+    (2..n).for_each(|i| {
+        cost[i] = std::cmp::min(
+            cost[i - 1] + (h[i - 1] as isize - h[i] as isize).abs() as usize,
+            cost[i - 2] + (h[i - 2] as isize - h[i] as isize).abs() as usize,
+        );
+    });
+
+    println!("{}", cost[n - 1]);
 }
