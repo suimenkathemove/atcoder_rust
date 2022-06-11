0ce4abb8399f82b432518f89b69bb530c47c74d6
emgniddikur <yoshiharu2580@gmail.com>
2022-06-11 13:51:08 +0900

    fix dp b

--- dp/src/bin/b.rs
+++ dp/src/bin/b.rs
@@ -2,16 +2,14 @@ fn main() {
     proconio::input! {
         n: usize,
         k: usize,
-        h: [usize; n],
+        h: [isize; n],
     }
 
     let mut cost = vec![0; n];
 
-    cost[1] = (h[0] as isize - h[1] as isize).abs() as usize;
-
-    (2..n).for_each(|i| {
+    (1..n).for_each(|i| {
         cost[i] = (std::cmp::max(i as isize - k as isize, 0) as usize..i)
-            .map(|j| cost[j] + (h[i] as isize - h[j] as isize).abs() as usize)
+            .map(|j| cost[j] + (h[j] - h[i]).abs())
             .min()
             .unwrap();
     });
