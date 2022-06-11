3cdf44521094e9b290bddd2d92d00c10c6568aa7
emgniddikur <yoshiharu2580@gmail.com>
2022-06-11 17:49:15 +0900

    dp h

--- dp/src/bin/h.rs
+++ dp/src/bin/h.rs
@@ -1,3 +1,48 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        l: usize,
+        x: [usize; n],
+        t1: usize,
+        t2: usize,
+        t3: usize,
+    }
+
+    let h = {
+        let mut h = vec![false; l + 1];
+        x.iter().for_each(|&i| {
+            h[i] = true;
+        });
+        h
+    };
+
+    let mut t = vec![std::usize::MAX; l + 1];
+
+    t[0] = 0;
+
+    (1..=l).for_each(|i| {
+        t[i] = std::cmp::min(t[i], t[i - 1] + t1);
+
+        if i >= 2 {
+            t[i] = std::cmp::min(t[i], t[i - 2] + t1 + t2);
+        }
+
+        if i >= 4 {
+            t[i] = std::cmp::min(t[i], t[i - 4] + t1 + 3 * t2);
+        }
+
+        if h[i] {
+            t[i] += t3;
+        }
+    });
+
+    (1..=3).for_each(|i| {
+        if l < i {
+            return;
+        }
+
+        t[l] = std::cmp::min(t[l], t[l - i] + t1 / 2 + (i - 1) * t2 + t2 / 2);
+    });
+
+    println!("{}", t[l]);
 }
