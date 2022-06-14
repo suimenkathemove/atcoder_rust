2a54fe30e4b69794f07f5ab44cf201e1b04b8fde
emgniddikur <yoshiharu2580@gmail.com>
2022-06-15 07:24:05 +0900

    abc161 d

--- abc161/src/bin/d.rs
+++ abc161/src/bin/d.rs
@@ -1,3 +1,34 @@
+use std::collections::VecDeque;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        k: usize,
+    }
+
+    let mut n = 0;
+
+    let mut q = VecDeque::<usize>::new();
+
+    (1..=9).for_each(|i| {
+        q.push_back(i);
+    });
+
+    while let Some(i) = q.pop_front() {
+        n += 1;
+
+        if n >= k {
+            println!("{}", i);
+            break;
+        }
+
+        let i_last = i % 10;
+
+        if i_last > 0 {
+            q.push_back(format!("{}{}", i, i_last - 1).parse().unwrap());
+        }
+        q.push_back(format!("{}{}", i, i_last).parse().unwrap());
+        if i_last < 9 {
+            q.push_back(format!("{}{}", i, i_last + 1).parse().unwrap());
+        }
+    }
 }
