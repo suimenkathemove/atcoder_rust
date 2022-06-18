77c5591bd2e5e4670d35db37e513df4db98c473b
emgniddikur <yoshiharu2580@gmail.com>
2022-06-19 01:12:22 +0900

    abc026 c

--- abc026/src/bin/c.rs
+++ abc026/src/bin/c.rs
@@ -1,3 +1,39 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+    }
+
+    let bs = {
+        let mut bs = vec![0, 0];
+        (2..=n).for_each(|_| {
+            proconio::input! {
+                b: usize,
+            }
+            bs.push(b);
+        });
+        bs
+    };
+
+    let mut s = vec![0; n + 1];
+
+    (1..=n).rev().for_each(|i| {
+        let children = bs
+            .iter()
+            .enumerate()
+            .filter_map(|(index, &b)| {
+                if b == i {
+                    return Some(index);
+                }
+                None
+            })
+            .collect::<Vec<_>>();
+        if children.is_empty() {
+            s[i] = 1;
+        } else {
+            let cs = children.iter().map(|&c| s[c]).collect::<Vec<_>>();
+            s[i] = cs.iter().max().unwrap() + cs.iter().min().unwrap() + 1;
+        }
+    });
+
+    println!("{}", s[1]);
 }
