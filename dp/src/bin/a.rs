19ad61a8b0800e9b69740707b7dccfde3d6295f2
emgniddikur <yoshiharu2580@gmail.com>
2022-06-21 05:49:18 +0900

    fix dp a

--- dp/src/bin/a.rs
+++ dp/src/bin/a.rs
@@ -8,13 +8,12 @@ fn main() {
 
     dp[0] = 0;
 
-    dp[1] = (h[1] - h[0]).abs();
+    (1..n).for_each(|i| {
+        dp[i] = dp[i].min(dp[i - 1] + (h[i] - h[i - 1]).abs());
 
-    (2..n).for_each(|i| {
-        dp[i] = std::cmp::min(
-            dp[i - 1] + (h[i] - h[i - 1]).abs(),
-            dp[i - 2] + (h[i] - h[i - 2]).abs(),
-        );
+        if i >= 2 {
+            dp[i] = dp[i].min(dp[i - 2] + (h[i] - h[i - 2]).abs());
+        }
     });
 
     println!("{}", dp[n - 1]);
